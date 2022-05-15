3bf8bcab58d9a66e23d82a3c35ace462e9a1e590
Damon Bayer <damon@damonbayer.com>
2022-05-14 19:50:39 -0700

    Revise pp plots

--- scripts/plots/pp_plots.R
+++ scripts/plots/pp_plots.R
@@ -18,12 +18,10 @@ dat <-
 
 max_date <- dat %>% filter(time == max_t) %>% pull(date)
 
-
 dat_tidy <-
   dat %>% 
   select(date, cases, tests, deaths) %>% 
   mutate(test_positivity = cases / tests) %>% 
-  select(-tests) %>% 
   pivot_longer(-date)
 
 model_table <-
@@ -44,6 +42,22 @@ all_pp <-
   unnest(pp_data) %>% 
   filter(date <= max_date)
 
+
+main_pp <- 
+  all_pp %>% 
+  filter(constant_alpha == F,
+         constant_IFR == F,
+         constant_R0 == F,
+         double_IFR_0 == F,
+         half_alpha_0 == F,
+         half_R0_0 == F,
+         half_S_0 == F,
+         use_seroprev == T,
+         use_tests == T) %>% 
+  select(date, name, value, starts_with("."))
+
+
+# Sensitivity Plots -------------------------------------------------------
 pp_test_pos_sensitivity_plot <- 
   all_pp %>% 
   filter(name == "test_positivity") %>% 
@@ -74,57 +88,14 @@ pp_death_sensitivity_plot <-
 
 
 
-pp_test_pos_plot <-
-  all_pp %>% 
-    filter(constant_alpha == F,
-           constant_IFR == F,
-           constant_R0 == F,
-           double_IFR_0 == F,
-           half_alpha_0 == F,
-           half_R0_0 == F,
-           half_S_0 == F,
-           use_seroprev == T,
-           use_tests == T) %>% 
-  filter(name == "test_positivity") %>% 
-  ggplot(aes(date, value)) +
-  geom_lineribbon(mapping = aes(ymin = .lower,
-                                ymax = .upper)) +
-  geom_point(data = dat_tidy %>% filter(name == "test_positivity")) +
-  scale_y_continuous(name = "Test Positivity", labels = percent) +
-  scale_x_date(name = "Date") +
-  ggtitle("Posterior Predictive Test Positivity") +
-  my_theme + theme(legend.position = "right")
-
-pp_deaths_plot <-
-  all_pp %>% 
-  filter(constant_alpha == F,
-         constant_IFR == F,
-         constant_R0 == F,
-         double_IFR_0 == F,
-         half_alpha_0 == F,
-         half_R0_0 == F,
-         half_S_0 == F,
-         use_seroprev == T,
-         use_tests == T) %>% 
-  filter(name == "deaths") %>% 
-  ggplot(aes(date, value)) +
-  geom_lineribbon(mapping = aes(ymin = .lower,
-                                ymax = .upper)) +
-  geom_point(data = dat_tidy %>% filter(name == "deaths")) +
-  scale_y_continuous(name = "Deaths", labels = comma) +
-  scale_x_date(name = "Date") +
-  ggtitle("Posterior Predictive Deaths") +
-  my_theme + theme(legend.position = "right")
-
-
-c("pp_death_sensitivity_plot", "pp_deaths_plot", "pp_test_pos_plot", 
-  "pp_test_pos_sensitivity_plot") %>% 
+c("pp_death_sensitivity_plot", "pp_test_pos_sensitivity_plot") %>% 
   walk(~save_plot_target_asp(filename = path("figures/advancement_slides", ., ext = "pdf"),
                              plot = get(.),
                              base_asp = 32/9,
                              base_height = 4))
 
 
+# Abstract Plot -----------------------------------------------------------
 tmp_xlim <- c(lubridate::ymd("2020-06-07"), lubridate::ymd("2020-08-23"))
 tmp_ylim <- c(1402, 8287)
 tmp_asp <- as.numeric((tmp_xlim[2] - tmp_xlim[1]) / (tmp_ylim[2] - tmp_ylim[1]))
@@ -144,3 +115,81 @@ pp_abstract_plot <-
   coord_fixed(ratio = tmp_asp, xlim = tmp_xlim, ylim = tmp_ylim)
 
 save_plot(filename = "~/Desktop/pp_abstract.pdf", plot = pp_abstract.pdf, base_asp = 1)
+
+
+# Main Plot ---------------------------------------------------------------
+library(patchwork)
+library(tidyverse)
+source("src/plot_functions.R")
+
+date_breaks <- "3 months"
+date_labels <- "%b %y"
+line_size <- 1
+point_size <- line_size 
+
+pp_tests_plot <- 
+  ggplot(mapping = aes(date, value)) +
+  geom_line(data = dat_tidy %>% 
+              filter(name == "tests"))+
+  geom_point(data = dat_tidy %>% 
+               filter(name == "tests")) +
+  scale_y_continuous(name = "Tests", labels = comma) +
+  scale_x_date(name = "Date", date_breaks = date_breaks, date_labels = date_labels) +
+  guides(fill = "none")
+
+
+pp_cases_plot <- 
+  ggplot(mapping = aes(date, value)) +
+  geom_lineribbon(data = main_pp %>% 
+                    filter(name == "cases"),
+                  mapping = aes(ymin = .lower,
+                                ymax = .upper),
+                  color = brewer_line_color, key_glyph = "rect") +
+  geom_point(data = dat_tidy %>% 
+               filter(name == "cases")) +
+  scale_y_continuous(name = "Cases", labels = comma) +
+  scale_x_date(name = "Date", date_breaks = date_breaks, date_labels = date_labels) +
+  my_theme +
+  theme(legend.position = c(0.25, 0.75))
+
+pp_deaths_plot <- 
+  ggplot(mapping = aes(date, value)) +
+  geom_lineribbon(data = main_pp %>% 
+                    filter(name == "deaths"),
+                  mapping = aes(ymin = .lower,
+                                ymax = .upper),
+                  color = brewer_line_color) +
+  geom_point(data = dat_tidy %>% 
+               filter(name == "deaths")) +
+  scale_y_continuous(name = "Deaths", labels = comma) +
+  scale_x_date(name = "Date", date_breaks = date_breaks, date_labels = date_labels) +
+  scale_fill_brewer() +
+  guides(fill = "none")
+
+pp_test_pos_plot <- 
+  ggplot(mapping = aes(date, value)) +
+  geom_lineribbon(data = main_pp %>% 
+                    filter(name == "test_positivity"),
+                  mapping = aes(ymin = .lower,
+                                ymax = .upper),
+                  color = brewer_line_color,) +
+  geom_point(data = dat_tidy %>% 
+               filter(name == "test_positivity")) +
+  scale_y_continuous(name = "Testing Positivity", labels = percent) +
+  scale_x_date(name = "Date", date_breaks = date_breaks, date_labels = date_labels) +
+  scale_fill_brewer() +
+  guides(fill = "none")
+
+main_pp_plot <- 
+  pp_tests_plot +
+  pp_cases_plot +
+  pp_deaths_plot +
+  pp_test_pos_plot +
+  patchwork::plot_layout(ncol = 2, nrow = 2) +
+  patchwork::plot_annotation(title = "Posterior Predictive")
+
+save_plot_target_asp(filename = "figures/advancement_slides/main_pp_plot.pdf",
+                     plot = main_pp_plot,
+                     ncol = 2,
+                     nrow = 2,
+                     base_asp = 16/9, base_height = 3)
