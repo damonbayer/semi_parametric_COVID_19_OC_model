314389c664a88ccf821fd36ab816e661142fa7af
Damon Bayer <damon@damonbayer.com>
2022-12-05 16:09:04 -0800

    update for julia 1.8.3

--- archive/precision_experiment.sh
+++ archive/precision_experiment.sh
@@ -5,14 +5,14 @@
 #SBATCH -N 1          ## run on a single node
 #SBATCH -n 1          ## request 4 tasks (4 CPUs)
 #SBATCH -t 04:00:00   ## 1 hr run time limit
-#SBATCH --mem=4G 
+#SBATCH --mem=4G
 #SBATCH -o precision_experiment-%A-%a.out
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=bayerd@uci.edu
 #SBATCH --array=1-24
 
 module purge
-module load julia/1.8.2
+module load julia/1.8.3
 cd //dfs6/pub/bayerd/semi_parametric_COVID_19_OC_model/
 
 julia --project --threads 1 scripts/precision_experiment.jl $SLURM_ARRAY_TASK_ID
--- slurm_submissions/generate_posterior_predictive_and_generated_quantities.sh
+++ slurm_submissions/generate_posterior_predictive_and_generated_quantities.sh
@@ -5,14 +5,14 @@
 #SBATCH -N 1          ## run on a single node
 #SBATCH -n 1          ## request 4 tasks (4 CPUs)
 #SBATCH -t 00:20:00   ## 1 hr run time limit
-#SBATCH --mem=8G 
+#SBATCH --mem=8G
 #SBATCH -o generate_posterior_predictive_and_generated_quantities-%A-%a.out
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=bayerd@uci.edu
 #SBATCH --array=113-135
 
 module purge
-module load julia/1.8.2
+module load julia/1.8.3
 cd //dfs6/pub/bayerd/semi_parametric_COVID_19_OC_model/
 
 julia --project --threads 1 scripts/generate_posterior_predictive_and_generated_quantities.jl $SLURM_ARRAY_TASK_ID
