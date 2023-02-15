6aa7fb605fa8c9c20dda18ce9de316ebb68aa7f7
Damon Bayer <damon@damonbayer.com>
2023-02-15 08:50:15 -0800

    function to format priors for manuscript

--- scripts/manuscript_utils/format_priors_for_table.R
+++ scripts/manuscript_utils/format_priors_for_table.R
@@ -1,32 +1,37 @@
+library(tidyverse)
+library(scales)
 expit <- function(x)  1/(1+exp(-x))
 logit <- function(x) -log(1/x - 1)
-my_formatter <- scales::number_format(accuracy = 0.0001)
 
-
-signif(6 - log(2), 3)
-format_for_table <- function(x) {
-  x_trans <- signif(x, 3)
-  str_c("\\makecell{", 
-        x_trans[1], 
-        " \\\\ (", 
-        x_trans[2],
-        ", ",
-        x_trans[3],
-        ")}")
+format_for_table <- function(dist_type = "log", dist_mean, dist_sd, interval_width = 0.95) {
+  tail_width <- (1 - interval_width) / 2 
+  raw_quantiles <- qnorm(p = c(0.5, tail_width, 1 - tail_width), mean = dist_mean, sd = dist_sd)
+  if (dist_type == "log") {
+    trans_quantiles <- exp(raw_quantiles)
+    distribuion_name <- "Log-Normal"
+  } else if (dist_type == "logit") {
+    trans_quantiles <- expit(raw_quantiles)
+    distribuion_name <- "Logit-Normal"
+  }
+  rounded_dist_mean <- dist_mean %>% signif(3) %>% format(scientific = FALSE)
+  rounded_dist_var <- dist_sd^2 %>% signif(3) %>% format(scientific = FALSE)
+  rounded_quantiles <- trans_quantiles %>% signif(3) %>% format(scientific = FALSE)
+  str_c(
+    distribuion_name,
+    "(",
+    rounded_dist_mean,
+    ", ",
+    rounded_dist_var,
+    ")",
+    " & ",
+    "\\makecell{", 
+    rounded_quantiles[1], 
+    " \\\\ (", 
+    rounded_quantiles[2],
+    ", ",
+    rounded_quantiles[3],
+    ")}") %>% 
+    cat()
 }
 
-
-
-
-format_for_table(expit(qnorm(p = c(0.5, 0.025, 0.975), mean = 6 - log(2), sd = 0.5))) |> cat()
-  
-n <- 10000
-
-tibble(alpha = rnorm(n = n, mean = 1.35, sd = 0.1),
-         R0 = rnorm(n, mean = 0, sd = 0.25),
-         IFR = rnorm(n, mean = -5.3, sd = 0.2)) |> 
-  pivot_longer(everything(), names_to = "parameter", values_to = "y") |> 
-  mutate(y = if_else(parameter == "IFR", expit(y), exp(y))) |> 
-  ggplot(aes(y)) +
-  facet_wrap(~parameter, scales = "free")+
-  stat_halfeye(normalize = "panels")
+format_for_table(dist_type = "log", dist_mean =  1.35 - log(2), dist_sd = 0.11)
